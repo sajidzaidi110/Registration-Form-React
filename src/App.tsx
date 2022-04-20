d828246140a712cf901c718d6a11100c76e62319
Sajid Zaidi <sajidzaidi@thequeno.com>
2022-04-21 02:42:06 +0500

    form complryrf

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,34 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from 'yup';
 
 function App() {
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
     return (
         <Formik
             initialValues={{
                 firstName: '',
                 lastName: '',
                 email: '',
-                password: '',
-                confirmPassword: ''
+                phone: '',
+                fatherfirstName : '',
+                fatherlastName : ''
             }}
             validationSchema={Yup.object().shape({
                 firstName: Yup.string()
                     .required('First Name is required'),
+                fatherlastName: Yup.string()
+                .required('Father Last Name is required'),
                 lastName: Yup.string()
                     .required('Last Name is required'),
+                fatherfirstName: Yup.string()
+                .required('Father First Name is required'),
                 email: Yup.string()
                     .email('Email is invalid')
                     .required('Email is required'),
-                password: Yup.string()
-                    .min(6, 'Password must be at least 6 characters')
-                    .required('Password is required'),
-                confirmPassword: Yup.string()
-                    .oneOf([Yup.ref('password'), null], 'Passwords must match')
-                    .required('Confirm Password is required')
+                phone: Yup.string()
+                .required("rPhone is equired")
+                .matches(phoneRegExp, 'Phone number is not valid')
+                .min(10, "Phone is incmplete")
+                .max(11, "Invalid Phone")
             })}
             onSubmit={fields => {
                 alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
@@ -32,37 +37,35 @@ function App() {
             render={({ errors, status, touched }) => (
                 <Form>
                     <div className="row">
+                        <div className="col-md-12">
+                            <h4>Personal Information</h4>
+                        </div>
+                    </div>
+                    <div className="row">
                         <div className="col-md-6 mr-10 form-group">
-                            <label htmlFor="firstName">First Name</label>
+                            <label htmlFor="firstName">First Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
                             <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
                             <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
                         </div>
                         <div className="col-md-6 form-group">
-                            <label htmlFor="lastName">Last Name</label>
+                            <label htmlFor="lastName">Last Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
                             <Field name="lastName" type="text" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
                             <ErrorMessage name="lastName" component="div" className="invalid-feedback" />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 mr-10 form-group">
-                            <label htmlFor="email">Email</label>
+                            <label htmlFor="email">Email<span className="ml-1" style={{color : 'red',}}>*</span></label>
                             <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
                             <ErrorMessage name="email" component="div" className="invalid-feedback" />
                         </div>
                         <div className="col-md-6">
-                            <label htmlFor="password">Password</label>
-                            <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
-                            <ErrorMessage name="password" component="div" className="invalid-feedback" />
+                            <label htmlFor="phone">Phone<span className="ml-1" style={{color : 'red',}}>*</span></label>
+                            <Field name="phone" type="number" className={'form-control' + (errors.phone && touched.phone ? ' is-invalid' : '')} />
+                            <ErrorMessage name="phone" component="div" className="invalid-feedback" />
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-md-6 col-sm-6">
-                            <div className="form-group">
-                                <label htmlFor="confirmPassword">Confirm Password</label>
-                                <Field name="confirmPassword" type="password" className={'form-control' + (errors.confirmPassword && touched.confirmPassword ? ' is-invalid' : '')} />
-                                <ErrorMessage name="confirmPassword" component="div" className="invalid-feedback" />
-                            </div>
-                        </div>
                         <div className="col-md-6 col-sm-6">
                             <div className="form-group">
                                 <label htmlFor="form-contact-country">Gender</label>
@@ -72,6 +75,67 @@ function App() {
                                     <option>world</option>
                                 </select>
                             </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h4>Parents Information</h4>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-6 mr-10 form-group">
+                            <label htmlFor="fatherfirstName">Father First Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
+                            <Field name="fatherfirstName" type="text" className={'form-control' + (errors.fatherfirstName && touched.fatherfirstName ? ' is-invalid' : '')} />
+                            <ErrorMessage name="fatherfirstName" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="col-md-6 form-group">
+                            <label htmlFor="fatherlastName">Father Last Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
+                            <Field name="fatherlastName" type="text" className={'form-control' + (errors.fatherlastName && touched.fatherlastName ? ' is-invalid' : '')} />
+                            <ErrorMessage name="fatherlastName" component="div" className="invalid-feedback" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 mr-10 form-group">
+                            <label htmlFor="firstName">Mother First Name</label>
+                            <Field name="firstName" type="text" className={'form-control'} />
+                        </div>
+                        <div className="col-md-6 form-group">
+                            <label htmlFor="lastName">Mother Last Name</label>
+                            <Field name="lastName" type="text" className={'form-control'} />
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h4>Last Education Information</h4>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 mr-10 form-group">
+                            <label htmlFor="class">Class</label>
+                            <Field name="class" type="text" className={'form-control'} />
+                        </div>
+                        <div className="col-md-6 form-group">
+                            <label htmlFor="grade">Grade</label>
+                            <Field name="grade" type="text" className={'form-control'} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 mr-10 form-group">
+                            <label htmlFor="instituition">Institute</label>
+                            <Field name="instituition" type="text" className={'form-control'} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 mr-10 form-group">
+                            <label htmlFor="yop">Year of passing</label>
+                            <Field name="yop" type="text" className={'form-control'} />
+                        </div>
+                        <div className="col-md-6 form-group">
+                            <label htmlFor="duration">Duration</label>
+                            <Field name="duration" type="text" className={'form-control'} />
                         </div>
                     </div>
                     <div className="form-group">
