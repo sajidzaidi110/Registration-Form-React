b88c512aabb9f0b7390099f57e70a1a115b7bcf1
Sajid Zaidi <sajidzaidi@thequeno.com>
2022-04-21 14:11:32 +0500

    form changes

--- src/App.tsx
+++ src/App.tsx
@@ -38,30 +38,31 @@ function App() {
                 <Form>
                     <div className="row">
                         <div className="col-md-12">
-                            <h4>Personal Information</h4>
+                            <h5>Personal Information</h5>
+                            <hr />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 mr-10 form-group">
                             <label htmlFor="firstName">First Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
-                            <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
+                            <Field name="firstName" placeholder="Enter First Name" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
                             <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
                         </div>
                         <div className="col-md-6 form-group">
                             <label htmlFor="lastName">Last Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
-                            <Field name="lastName" type="text" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
+                            <Field name="lastName" type="text" placeholder="Enter Last Name" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
                             <ErrorMessage name="lastName" component="div" className="invalid-feedback" />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 mr-10 form-group">
                             <label htmlFor="email">Email<span className="ml-1" style={{color : 'red',}}>*</span></label>
-                            <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
+                            <Field name="email" type="text" placeholder="Enter Email" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
                             <ErrorMessage name="email" component="div" className="invalid-feedback" />
                         </div>
                         <div className="col-md-6">
                             <label htmlFor="phone">Phone<span className="ml-1" style={{color : 'red',}}>*</span></label>
-                            <Field name="phone" type="number" className={'form-control' + (errors.phone && touched.phone ? ' is-invalid' : '')} />
+                            <Field name="phone" type="number" placeholder="Enter Phone Number" className={'form-control' + (errors.phone && touched.phone ? ' is-invalid' : '')} />
                             <ErrorMessage name="phone" component="div" className="invalid-feedback" />
                         </div>
                     </div>
@@ -77,65 +78,67 @@ function App() {
                             </div>
                         </div>
                     </div>
-
+                    <hr />
                     <div className="row">
                         <div className="col-md-12">
-                            <h4>Parents Information</h4>
+                            <h5>Parents Information</h5>
+                            <hr />
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col-md-6 mr-10 form-group">
                             <label htmlFor="fatherfirstName">Father First Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
-                            <Field name="fatherfirstName" type="text" className={'form-control' + (errors.fatherfirstName && touched.fatherfirstName ? ' is-invalid' : '')} />
+                            <Field name="fatherfirstName" placeholder="Enter Father First Name" type="text" className={'form-control' + (errors.fatherfirstName && touched.fatherfirstName ? ' is-invalid' : '')} />
                             <ErrorMessage name="fatherfirstName" component="div" className="invalid-feedback" />
                         </div>
                         <div className="col-md-6 form-group">
                             <label htmlFor="fatherlastName">Father Last Name<span className="ml-1" style={{color : 'red',}}>*</span></label>
-                            <Field name="fatherlastName" type="text" className={'form-control' + (errors.fatherlastName && touched.fatherlastName ? ' is-invalid' : '')} />
+                            <Field name="fatherlastName" type="text" placeholder="Enter Father Last Name" className={'form-control' + (errors.fatherlastName && touched.fatherlastName ? ' is-invalid' : '')} />
                             <ErrorMessage name="fatherlastName" component="div" className="invalid-feedback" />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 mr-10 form-group">
                             <label htmlFor="firstName">Mother First Name</label>
-                            <Field name="firstName" type="text" className={'form-control'} />
+                            <Field name="firstName" type="text" placeholder="Enter Mother First Name" className={'form-control'} />
                         </div>
                         <div className="col-md-6 form-group">
                             <label htmlFor="lastName">Mother Last Name</label>
-                            <Field name="lastName" type="text" className={'form-control'} />
+                            <Field name="lastName" type="text" placeholder="Enter Mother Last Name" className={'form-control'} />
                         </div>
                     </div>
-
+                    <hr />
                     <div className="row">
                         <div className="col-md-12">
-                            <h4>Last Education Information</h4>
+                            <h5>Last Education Information</h5>
+                            <hr />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 mr-10 form-group">
                             <label htmlFor="class">Class</label>
-                            <Field name="class" type="text" className={'form-control'} />
+                            <Field name="class" placeholder="Enter Class" type="text" className={'form-control'} />
                         </div>
                         <div className="col-md-6 form-group">
                             <label htmlFor="grade">Grade</label>
-                            <Field name="grade" type="text" className={'form-control'} />
+                            <Field name="grade" placeholder="Enter Grade" type="text" className={'form-control'} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-12 mr-10 form-group">
                             <label htmlFor="instituition">Institute</label>
-                            <Field name="instituition" type="text" className={'form-control'} />
+                            <Field name="instituition"  placeholder="Enter Last Institute" type="text" className={'form-control'} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 mr-10 form-group">
                             <label htmlFor="yop">Year of passing</label>
-                            <Field name="yop" type="text" className={'form-control'} />
+                            <Field name="yop" type="text"  placeholder="Enter Passing Year"className={'form-control'} />
                         </div>
                         <div className="col-md-6 form-group">
                             <label htmlFor="duration">Duration</label>
-                            <Field name="duration" type="text" className={'form-control'} />
+                            <Field name="duration" type="text"  placeholder="Enter Duration" className={'form-control'} />
                         </div>
                     </div>
                     <div className="form-group">
